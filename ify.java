cd84728a4b533efc3f881d40d7df0991de802512
PhillzG <phillzgtwitch@gmail.com>
2024-01-24 23:04:05 +0100

    Nic

---
+++ ify.java
@@ -0,0 +1,16 @@
+public class ify {
+    public static void main(String[] args) {
+        int age = 2;
+        if(age>=75){
+            System.out.println("Oldy");
+
+        }
+        else if(age>=18){
+            System.out.println("Adult");
+
+        }
+        else{
+            System.out.println("Not an Adult");
+        }
+    }
+}
---
+++ switche.java
@@ -0,0 +1,26 @@
+public class switche {
+    public static void main(String[] args) {
+        String day = "ddd";
+        switch(day){
+            case "Sunday": System.out.println("It's Sunday");
+            break;
+            case "Monday": System.out.println("It's Monday");
+            break;
+            case "Tuesday": System.out.println("It's Tuesday");
+            break;
+            case "Wensday": System.out.println("It's Wensday");
+            break;
+            case "Saturday": System.out.println("It's Saturday");
+            break;
+            case "Thursday": System.out.println("It's Thursday");
+            break;
+            case "Friday": System.out.println("It's Friday");
+            break;
+            default: System.out.println("Error");
+           
+        
+
+
+        }
+    }
+}
