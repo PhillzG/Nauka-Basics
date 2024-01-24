2122cf624e8431cc13dbaf2d8dbf83d30f1951b7
PhillzG <phillzgtwitch@gmail.com>
2024-01-24 23:36:12 +0100

    Update PrzykladyKol.java

--- PrzykladyKol.java
+++ PrzykladyKol.java
@@ -1,27 +1,22 @@
-public class PrzykladyKol {
-    public static void main(String[] args) {
-        byte a = 40, b = 50;
-        byte suma = (byte) ((byte)a+b);                                               //Błąd w oryginalnej analize kodu, trzeba zmienic typ byte
-        System.out.println(suma);
-        System.out.println("");
+
+        // byte a = 40, b = 50;
+        // byte suma = (byte) ((byte)a+b);                                               //Błąd w oryginalnej analize kodu, trzeba zmienic typ byte
+        // System.out.println(suma);
+        // System.out.println("");
 
 
-        System.out.println("II");
-        System.out.println("");
-        int x = 4;
-        long y =x*4-x++;
-        if(y<10)System.out.println("za malo");                                      // printuje "w sam raz"  y = 12
-        else System.out.println("W sam raz");  
+       
+        // int x = 4;
+        // long y =x*4-x++;
+        // if(y<10)System.out.println("za malo");                                      // printuje "w sam raz"  y = 12
+        // else System.out.println("W sam raz");  
         
                                      
-        System.out.println("");
-        System.out.println("III");
-        int c = 0;
-        double d;
-        if(a>=0) d =1.701;
-        if(a<0) d=2D*3.14f;
-        System.out.println(d);
+       
+        // int c = 1;
+        // double d;                                                                     // Błąd? Chyba
+        // if(a>=0) d =1.701;
+        // if(a<0) d=2D*3.14f;
+        // System.out.println(d);
 
-    }
     
-}
