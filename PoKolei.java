26337579cd92b51e368ac2382e24724a6d40a1b5
PhillzG <phillzgtwitch@gmail.com>
2024-01-25 00:28:19 +0100

    Create PoKolei.java

---
+++ PoKolei.java
@@ -0,0 +1,9 @@
+public class PoKolei {
+    public static void main(String[] args) {
+        int x = 150;
+        for(int i =0;i<x+1;i++)                         //print wszystkiego po kolei(+1 aby bylo do 150 a nie do 149)
+            System.out.println(i);
+
+
+    }
+}
